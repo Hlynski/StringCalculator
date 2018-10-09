53f619033212288d1996625abad458471a47698c
Hlynski <hlynurv16@ru.is>
2018-10-09 13:12:53 +0000

    made some refactoring

--- src/calculator.js
+++ src/calculator.js
@@ -5,17 +5,21 @@ function add(numbers){
 	}
 	if(numbers.includes(",")){
 		var numbArr = numbers.split(",");
-		var total = 0;
-
-		for(var i = 0; i < numbArr.length; i++){
-			total += parseInt(numbArr[i]);
-			
-		}
-		return total;
+		
+		return sum(numbArr);
 		
 	}
 	else{
 		return parseInt(numbers);
 	}
 }
+function sum(numbArr){
+	var total = 0;
+
+		for(var i = 0; i < numbArr.length; i++){
+			total += parseInt(numbArr[i]);
+		}
+		return total;
+}
+
 module.exports = add;
