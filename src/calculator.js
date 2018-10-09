a690914dca8471e954c439c0f53dc78065c34ee0
Hlynski <hlynurv16@ru.is>
2018-10-09 13:01:07 +0000

    function for adding two numbers successfull

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,12 @@
 function add(numbers){
+	var numbArr;
+
 	if(numbers == ""){
 		return 0;
+	}
+	else if(numbers.includes(",")){
+		numbArr = numbers.split(",");
+		return parseInt(numbArr[0]) + parseInt(numbArr[1]);
 	}
 	return parseInt(numbers);
 }
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,9 +1,13 @@
 const add = require('./calculator');
 
-test("it should return 0", () => {
+test("it should return 0 from an empty string", () => {
 	expect(add("")).toBe(0);
 });
 
-test("it should be 2", () => {
+test("it should return 2", () => {
 	expect(add("2")).toBe(2);
+});
+
+test("it should add the 2 numbers and return the sum", () => {
+	expect(add("1,2")).toBe(3);
 });
